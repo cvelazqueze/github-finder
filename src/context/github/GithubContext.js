0e788e6f355412ecfb6daa1e60aa96e4b3eb469c
Cesar Velzaquez <cesarve@mty.ipointsystems.com>
2022-10-05 11:56:43 -0500

    Adding Working Search bar and prunning functions

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
 
@@ -8,27 +9,61 @@ const {
 }= process.env
 
 export const GithubProvider = ({children}) => {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const initialState = {
+        users: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
 
     const fetchUsers = async () => {
-        const response = await fetch(`${REACT_APP_GITHUB_URL}/users`, {
-          headers: {
-            Authorization: `token ${REACT_APP_GITHUB_TOKEN}`
-          }
-        })
+      setLoading()
+      
+      const response = await fetch(`${REACT_APP_GITHUB_URL}/users`, {
+        headers: {
+          Authorization: `token ${REACT_APP_GITHUB_TOKEN}`
+        }
+      })
     
-        const data = await response.json()
-        
-        setUsers(data)
-        setLoading(false)
+      const data = await response.json()
+      
+      dispatch({
+          type: 'GET_USERS',
+          payload: data,
+      })
+    }
     
-      }
+    const searchUsers = async (text) => {
+      setLoading()
+
+      const params = new URLSearchParams({
+        q:text
+      })
+      
+      const response = await fetch(`${REACT_APP_GITHUB_URL}/search/users?${params}`, {
+        headers: {
+          Authorization: `token ${REACT_APP_GITHUB_TOKEN}`
+        }
+      })
+    
+      const {items} = await response.json()
+      
+      dispatch({
+          type: 'GET_USERS',
+          payload: items,
+      })
+    }
+
+    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
+
+      const setLoading = () => dispatch({type: 'SET_LOADING'})
 
       return <GithubContext.Provider value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
+        searchUsers,
+        clearUsers,
       }}>
         {children}
       </GithubContext.Provider>
